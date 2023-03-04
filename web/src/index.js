694081b1cb0487941d076138400e1e52c53bc2f4
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2023-03-04 14:55:45 -0300

    feat: Adiciona Context para o Carrinho

--- web/src/index.js
+++ web/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { SaleProvider } from './context/SaleContext.js';
 import App from './App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <React.StrictMode>
+    <SaleProvider>
       <App />
-    </React.StrictMode>
+    </SaleProvider>
   </BrowserRouter>,
 );
