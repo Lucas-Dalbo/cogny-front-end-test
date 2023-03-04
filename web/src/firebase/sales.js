38530a76f039a9ad4becfbcd01214a0b13ac02e0
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2023-03-04 17:43:15 -0300

    feat: Funcionalidades de Cart - O pedido é salvo no banco de dados apropriadamente

--- web/src/firebase/sales.js
+++ web/src/firebase/sales.js
@@ -1,8 +1,8 @@
-import { collection, setDoc } from 'firebase/firestore';
+import { collection, addDoc } from 'firebase/firestore';
 import { db } from '../firebase-config.js';
 
 const sales = collection(db, 'sales');
 
-export const saveSale = async (doc) => {
-  await setDoc(sales, doc);
+export const saveSale = async (doc, total) => {
+  await addDoc(sales, { products: doc, total });
 };
--- web/src/pages/Cart.js
+++ web/src/pages/Cart.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import NavBar from '../components/NavBar.js';
 import { SaleContext } from '../context/SaleContext.js';
 import { saveSale } from '../firebase/sales.js';
@@ -7,28 +7,67 @@ import CartCard from '../components/CartCard.js';
 
 export default function Cart() {
   const { itens, setItens, local } = useContext(SaleContext);
-  console.log(itens, setItens, local);
+  const [total, setTotal] = useState(0);
 
   const onClick = async () => {
-    await saveSale(itens);
+    try {
+      await saveSale(itens, total).then(() => {
+        alert('Sua pedido foi realizado com sucesso');
+      });
+      localStorage.removeItem(local);
+      setItens([]);
+    } catch (e) {
+      console.log(e);
+      alert('Erro. Por uma falha do sistema, seu pedido não foi registrado');
+    }
   };
-  console.log(onClick);
+
+  useEffect(() => {
+    const calculateTotal = () => {
+      const sum = itens.reduce((acc, crr) => {
+        acc += (crr.price * crr.quantity);
+        return acc;
+      }, 0);
+
+      setTotal(sum);
+    };
+
+    calculateTotal();
+  }, [itens]);
+
   return (
     <div>
       <NavBar />
       <main className={ styles.container }>
-        <div className={ styles.header }>
-          <h3>PRODUTO</h3>
-          <h3>QTD</h3>
-          <h3>PREÇO</h3>
-        </div>
         {
-          itens.length !== 0 ? (
+          itens.length > 0 && (
+            <div className={ styles.header }>
+              <h3>PRODUTO</h3>
+              <h3>QTD</h3>
+              <h3>PREÇO</h3>
+            </div>
+          )
+        }
+        {
+          itens.length > 0 ? (
             itens.map((p, id) => (
               <CartCard key={ id } product={ p } />
             ))
           ) : (
             <h1>Ainda sem produtos no carrinho!</h1>
+          )
+        }
+        {
+          itens.length !== 0 && (
+            <div className={ styles.bottom }>
+              <button onClick={ onClick }>
+                FINALIZAR PEDIDO
+              </button>
+              <div>
+                <p>TOTAL</p>
+                <h2>{`R$ ${total.toFixed(2).replace('.', ',')}`}</h2>
+              </div>
+            </div>
           )
         }
       </main>
