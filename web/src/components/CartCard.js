7bd13b9eb2bf92a419a3407a58aac5b6ff4f71ad
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2023-03-22 10:14:34 -0300

    style: Aumenta resposividade da pagina de Carrinho

--- web/src/components/CartCard.js
+++ web/src/components/CartCard.js
@@ -6,7 +6,7 @@ export default function CartCard({ product, id, addOne, removeOne, removeAll }) 
   const { name, image, price, quantity } = product;
 
   return (
-    <div>
+    <div className={ styles.container }>
       <div className={ styles.card }>
         <div className={ styles.info }>
           <img src={ image } alt={ name } />
@@ -15,37 +15,39 @@ export default function CartCard({ product, id, addOne, removeOne, removeAll }) 
             <h3>{ `R$ ${price.toFixed(2).replace('.', ',')}` }</h3>
           </div>
         </div>
-        <div className={ styles.quant }>
+        <div className={ styles.options }>
+          <div className={ styles.quant }>
+            <button
+              className={ styles.quantBtn }
+              type="button"
+              value={ id }
+              onClick={ addOne }
+            >
+              Δ
+            </button>
+            <p>{ quantity }</p>
+            <button
+              className={ styles.quantBtn }
+              type="button"
+              value={ id }
+              onClick={ removeOne }
+              disabled={ quantity === 1 }
+            >
+              ∇
+            </button>
+          </div>
+          <h3 className={ styles.total }>
+            { `R$ ${(price * quantity).toFixed(2).replace('.', ',')}` }
+          </h3>
           <button
-            className={ styles.quantBtn }
             type="button"
+            className={ styles.removeBtn }
             value={ id }
-            onClick={ addOne }
+            onClick={ removeAll }
           >
-            Δ
-          </button>
-          <p>{ quantity }</p>
-          <button
-            className={ styles.quantBtn }
-            type="button"
-            value={ id }
-            onClick={ removeOne }
-            disabled={ quantity === 1 }
-          >
-            ∇
+            <i className="fa fa-trash-o" />
           </button>
         </div>
-        <h3 className={ styles.total }>
-          { `R$ ${(price * quantity).toFixed(2).replace('.', ',')}` }
-        </h3>
-        <button
-          type="button"
-          className={ styles.removeBtn }
-          value={ id }
-          onClick={ removeAll }
-        >
-          <i className="fa fa-trash-o" />
-        </button>
       </div>
       <hr />
     </div>
